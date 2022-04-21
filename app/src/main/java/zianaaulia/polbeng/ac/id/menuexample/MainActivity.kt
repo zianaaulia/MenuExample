9912b6efd47d3f30aadbf8f827c98d7cd396eac0
Ziana Aulia <zianaaulia2602@gmail.com>
2022-04-21 11:31:42 +0700

    Implementasi Menu

--- app/src/main/java/zianaaulia/polbeng/ac/id/menuexample/MainActivity.kt
+++ app/src/main/java/zianaaulia/polbeng/ac/id/menuexample/MainActivity.kt
@@ -2,10 +2,43 @@ package zianaaulia.polbeng.ac.id.menuexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.menuFile -> {
+                showMessage("File Menu")
+                return true
+            }
+            R.id.menuEdit -> {
+                showMessage("File Edit")
+            }
+            R.id.menuHelp-> {
+                showMessage("File Help")
+            }
+            R.id.menuExit-> {
+                showMessage("File Exit")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showMessage(msg: String) {
+        Snackbar.make(root_layout, msg, Snackbar.LENGTH_LONG).show()
     }
 }
